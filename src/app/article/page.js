4155eac6d68080dd452aa8a1e689024b431b4d04
kou7306 <kouta.1206.y@icloud.com>
2024-01-09 18:06:37 +0900

    third

--- src/app/article/page.js
+++ src/app/article/page.js
@@ -23,14 +23,37 @@ export default async function Page() {
  
   return (
     <div className="article">
-      <h1>Qiita Items</h1>
-      <ul>
+      <h1>Article</h1>
+      <ul className='items'>
         {data.map((item) => (
-          <li key={item.id}>
-            <h2>{item.title}</h2>
+          <li key={item.id} className='item'>
+            <a href={item.url}>
+              <p className='date'>{formatDate(item.updated_at)}</p>
+              <h2>{item.title}</h2>
+              <p className='spans'>
+                {item.tags.map((tag) => (
+                  <span className='tag' key={tag.name}>{tag.name}</span>        
+                  ))}
+              </p>
+
+                
+                <p className='likes'><span className='heart'>♡</span>{item.likes_count}</p>
+            </a>
+            
           </li>
         ))}
       </ul>
     </div>
   );
+}
+
+
+// フォーマット関数
+function formatDate(dateTimeString) {
+  const dateObject = new Date(dateTimeString);
+  const year = dateObject.getFullYear();
+  const month = String(dateObject.getMonth() + 1).padStart(2, '0');
+  const day = String(dateObject.getDate()).padStart(2, '0');
+  
+  return `${year}年${month}月${day}日`;
 }
