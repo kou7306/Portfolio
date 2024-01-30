17ba6c57b58366c98d39ad93f482771d95f9d319
kou7306 <kouta.1206.y@icloud.com>
2024-01-31 02:18:28 +0900

    work-slider

--- src/app/get_works.js
+++ src/app/get_works.js
@@ -5,15 +5,15 @@ import Slider from './Slider';
 
 
 export default async function Get_works () {
-    // const works = await client.get({ endpoint: 'works' });
-    // const datas = works.contents;
-    // const images = [];
-    // datas.forEach((work) => {
-    //     // 画像のURLを取得して追加
+    const works = await client.get({ endpoint: 'works' });
+    const datas = works.contents;
+    const images = [];
+    datas.forEach((work) => {
+        // 画像のURLを取得して追加
         
-    //     images.push(work.work_imgs[0].url);
-    // });
-    const images=["https://placehold.jp/150x150.png"]
+        images.push(work.work_imgs[0].url);
+    });
+    // const images=["https://placehold.jp/150x150.png"]
 
     return (
         <Slider images={images}/>
