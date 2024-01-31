57c6d00b64ec799a946b79cb480f752dc2e014c6
kou7306 <kouta.1206.y@icloud.com>
2024-02-01 04:26:30 +0900

    detailtitle

--- src/app/AboutTop.js
+++ src/app/AboutTop.js
@@ -6,7 +6,7 @@ const AboutTop = () => {
   return (
     <div className='about-all'>
         <div className='about-contents'>
-            <h1 className='about-title'>ABOUT</h1>
+            <h1 className='about-title'>About</h1>
             <p className='about-detail'>coins21所属し、知能ロボット研究室で研究をする大学生。課外活動ではSTECHに所属し、ハッカソンなどのイベントに積極的に参加している。</p>
             <div className='about-button'>
                 <ViewMoreButton rel="about"/>
