2fb8968cbd254da8cbb61654fd229f9daecd19e0
kou7306 <kouta.1206.y@icloud.com>
2024-01-31 20:19:56 +0900

    career

--- src/app/about/Career2.js
+++ src/app/about/Career2.js
@@ -3,14 +3,21 @@ import './Career2.css';
 
 const Career2 = ({datas}) => {
   return (
-    <div className='careerAll'>
-        {datas.map((data) => (
-        <li className='careerCon'>
-          <h1 className='careerTopic'>{data.title}</h1>
-          <p className='careerDetail'>{data.detail}</p>
-        </li>
-      ))}
-    </div>
+    <div className='timeline'>
+      
+        {datas.map((data, index) => (
+          <li className={`careerCon ${index % 2 === 0 ? 'left-container' : 'right-container'}`}>
+            <img src="/images/pin.png"/>
+            <div className='text-box'>
+            <h1 className='careerTopic'>{data.title}</h1>
+            <small>{data.date}</small>
+            <p className='careerDetail'>{data.detail}</p>
+            </div>
+          </li>
+        ))}
+      </div>
+
+    
   )
 }
 
