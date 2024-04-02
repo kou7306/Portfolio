c80f7255ea4ce2e7b344ec4e809d177d95b25017
kou7306 <kouta.1206.y@icloud.com>
2024-04-02 21:57:20 +0900

    about(

--- src/app/AboutTop.js
+++ src/app/AboutTop.js
@@ -7,7 +7,7 @@ const AboutTop = () => {
     <div className='about-all'>
         <div className='about-contents'>
             <h1 className='about-title'>About</h1>
-            <p className='about-detail'>coins21所属し、知能ロボット研究室で研究をする（予定）大学生。課外活動ではSTECHに所属し、ハッカソンなどのイベントによく参加している。</p>
+            <p className='about-detail'>coins21所属し、知能ロボット研究室で研究をする大学生。課外活動ではSTECHに所属し、ハッカソンなどのイベントによく参加している。</p>
             <div className='about-button'>
                 <ViewMoreButton rel="about"/>
             </div>
